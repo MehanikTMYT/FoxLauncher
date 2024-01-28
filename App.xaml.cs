f8d33422b7b0bf3ad5c6ad76c8aa627c5ed5474e
MehanikTMYT <Pitter479@gmail.com>
2024-01-28 17:05:52 +0300

    Update App.xaml.cs
    fix

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,14 +3,4 @@ using System.Windows;
 
 namespace FoxLauncher
 {
-    public partial class App : System.Windows.Application
-    {
-        [STAThread]
-        public static void Main()
-        {
-            App app = new App();
-            MainWindow mainWindow = new MainWindow();
-            app.Run(mainWindow);
-        }
-    }
 }
